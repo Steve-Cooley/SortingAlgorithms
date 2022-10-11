fde9d410bf680dbcb3c83fc7df0a88dd7b8eaff8
Steve Cooley <scooley138@gmail.com>
2022-10-10 17:14:32 -0700

    Implement mergeSort and supporting methods

--- src/main/java/Sorters.java
+++ src/main/java/Sorters.java
@@ -6,7 +6,7 @@ public class Sorters {
     // **************** utils *****************
     // I thought a few simple utilities would be nice to avoid redundancy
 
-    //util: generate random array of ints
+    // util: generate random array of ints
     public static int[] genRandArray(int len, int max) {
         Random rand = new Random();
         List<Integer> numberList = new ArrayList<>();
@@ -24,15 +24,94 @@ public class Sorters {
         }
     }
 
+    /**
+     * Takes two already sorted arrays, combines them preserving
+     * that order, and returns the results. Will not work with
+     * unsorted arrays. Written for mergeSort, but may be used
+     * in elsewhere.
+     * 
+     * @param presortedLeft must be sorted
+     * @param presortedRight must be sorted
+     * @return merged array
+     */
+    public static int[] mergeSorted(int[] presortedLeft, int[] presortedRight) {
+        int leftLen = presortedLeft.length;
+        int rightlen = presortedRight.length;
+        int[] merged = new int[leftLen + rightlen];
+        int i = 0, j = 0, k = 0;
+        while (i < leftLen && j < rightlen) {
+            if (presortedLeft[i] <= presortedRight[j]) {
+                merged[k] = presortedLeft[i];
+                ++i;
+                ++k;
+            } else {
+                merged[k] = presortedRight[j];
+                ++j;
+                ++k;
+            }
+        }
+        while (i < leftLen) {
+            merged[k] = presortedLeft[i];
+            ++i;
+            ++k;
+        }
+        while (j < rightlen) { 
+            merged[k] = presortedRight[j];
+            ++j;
+            ++k;
+        }
+
+        return merged;
+    }
+
+    public static void testMergeSorted() {
+        System.out.println("Test results for mergeSorted");
+        int[] left = { 1, 3, 5, 23, 61, 62};
+        int[] right = { 2, 4, 6 , 22};
+        int[] merged = mergeSorted(left, right);
+        for (int num : merged) {
+            System.out.println(num);
+        }
+    }
 
     // ************* sorters and tests ***********************
 
     // merge sort O(nlogn)
-    // divide recursively til you get to one, which is inherently sorted. Then climb
-    // back up that
-    // tree, merging as you go.
-    public static int[  ] mergeSort(int[] inpArray) {
-        return inpArray;
+    // divide in half recursively til you get to one, which is inherently sorted.
+    // Then climb back up that tree, merging as you go.
+    public static int[] mergeSort(int[] inpArray) {
+        int len = inpArray.length;
+        // if the array is 1 or less, then it's already sorted, so return it. 
+        if (len < 2) {
+            return inpArray;
+        }
+        // create the two halves to be recursivley sorted
+        int midI = len/2;
+        int[] left = new int[midI];
+        int[] right = new int[len-midI];
+        //populate left half
+        for (int i = 0; i < midI; ++i){
+            left[i] = inpArray[i];
+        }
+        // populate right half  0 1 2 3 4
+        for (int i = 0; i < (len-midI); ++i){
+            right[i] = inpArray[i+midI];
+        }
+        return mergeSorted(mergeSort(left), mergeSort(right));
+        // todo: this is just to keep the compiler happy for now.
+        // return inpArray;
+    }
+
+    private static void testMergeSort(int[] testArray) {
+        // int[] testArray = {2, 4, 6, 1, 3, 5};
+        long start1 = System.nanoTime();
+        testArray = mergeSort(testArray);
+        long end1 = System.nanoTime();
+        System.out.println("mergeSort:     " + (end1-start1));
+        // System.out.println("Results of mergeSort: ");
+        // for (int num : testArray){
+        //     System.out.println(num);
+        // }
     }
 
     // bubble sort
@@ -49,20 +128,21 @@ public class Sorters {
                     int temp = inpArray[i];
                     inpArray[i] = inpArray[i + 1];
                     inpArray[i + 1] = temp;
-//                    System.out.println(String.format("swapped index %d with %d.", i, i + 1));
+                    // System.out.println(String.format("swapped index %d with %d.", i, i + 1));
                 }
                 ++forLoops;
             }
             whileLoops++;
-//            len--; // This might be fun to see what kind of speedup this would produce
+            // len--; // This might be fun to see what kind of speedup this would produce
         }
-//        System.out.println(String.format("while loops: %d for loops %d: ", whileLoops, forLoops));
+        // System.out.println(String.format("while loops: %d for loops %d: ",
+        // whileLoops, forLoops));
         return inpArray;
     }
 
     public static void testBubbleSort() {
         System.out.println("These are the results of bubbleSort:");
-//        int[] testArray = {2, 4, 6, 1, 3, 5};
+        // int[] testArray = {2, 4, 6, 1, 3, 5};
         int[] testArray = genRandArray(50, 5000);
         int[] bubbleResults = bubbleSort(testArray);
         for (int num : bubbleResults) {
@@ -77,10 +157,10 @@ public class Sorters {
         long start1 = System.nanoTime();
         int[] bubbleResults = bubbleSort(testArray);
         long end1 = System.nanoTime();
-        System.out.println("elapsed time in nanoseconds: " + (end1-start1));
-//        for (int num : bubbleResults) {
-//            System.out.println(num);
-//        }
+        System.out.println("elapsed time in nanoseconds: " + (end1 - start1));
+        // for (int num : bubbleResults) {
+        // System.out.println(num);
+        // }
     }
 
     public static void testBubbleSort(int[] testArray, boolean compact) {
@@ -88,7 +168,7 @@ public class Sorters {
         long start1 = System.nanoTime();
         int[] bubbleResults = bubbleSort(testArray);
         long end1 = System.nanoTime();
-        System.out.println("bubbleSort:    " + (end1-start1));
+        System.out.println("bubbleSort:    " + (end1 - start1));
 
     }
 
@@ -114,15 +194,14 @@ public class Sorters {
     }
 
     private static void testBogosort() {
-        int[] testArray = {2, 4, 6, 1, 3, 5};
-        int[] presorted = {1, 2, 3, 4, 5, 6};
+        int[] testArray = { 2, 4, 6, 1, 3, 5 };
+        int[] presorted = { 1, 2, 3, 4, 5, 6 };
         int[] bogoresults = bogosort(testArray, presorted);
         System.out.println("These are the results of bogosort: ");
         for (int i : bogoresults) {
             System.out.println(i);
         }
     }
-
 
     // selection sort
 
@@ -162,7 +241,7 @@ public class Sorters {
     }
 
     private static void testInsertionSort() {
-        int[] testArray = {2, 4, 6, 1, 3, 5};
+        int[] testArray = { 2, 4, 6, 1, 3, 5 };
         // This line actually sorts the testArray in place, which is not what I
         // intended. I don't really understand why it's doing that or how to stop it.
         int[] results = insertionSort(testArray);
@@ -180,7 +259,7 @@ public class Sorters {
         int[] results = insertionSort(testArray);
         long end1 = System.nanoTime();
         System.out.println("Printing results for insertionSort");
-        System.out.println("elapsed time in nanoseconds: " + (end1-start1));
+        System.out.println("elapsed time in nanoseconds: " + (end1 - start1));
     }
 
     private static void testInsertionSort(int[] testArray, boolean compact) {
@@ -189,12 +268,12 @@ public class Sorters {
         long start1 = System.nanoTime();
         int[] results = insertionSort(testArray);
         long end1 = System.nanoTime();
-        System.out.println("insertionSort: " + (end1-start1));
+        System.out.println("insertionSort: " + (end1 - start1));
     }
 
     public static void test() {
         System.out.println("Hello World! ");
-        int[] testArray = {1, 2, 3};
+        int[] testArray = { 1, 2, 3 };
         System.out.println("the length of the test array is: " + testArray.length);
     }
 
@@ -202,11 +281,15 @@ public class Sorters {
         int[] randomArray = genRandArray(100_000, 1000);
         testInsertionSort(Arrays.copyOf(randomArray, randomArray.length), true);
         // test Bogosort
-//        testBogosort();
+        // testBogosort();
         // test bubbleSort
-//        randomArray = genRandArray(1000, 1000);
+        // randomArray = genRandArray(1000, 1000);
         testBubbleSort(Arrays.copyOf(randomArray, randomArray.length), true);
-//        testGenRandomArray();
+        // testGenRandomArray();
+        // test mergeSorted
+        // testMergeSorted();
+        testMergeSort(Arrays.copyOf(randomArray, randomArray.length));
     }
-/// trying to get the pom.xml file to upload to github!!!
+
+
 }
